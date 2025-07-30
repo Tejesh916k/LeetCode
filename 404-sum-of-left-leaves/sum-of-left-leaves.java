755287a4c82eac05103bf8e099cfdc250e73c009
Kesanakurthy Tejesh sai manikanta <148753229+Tejesh916k@users.noreply.github.com>
2025-07-30 23:46:38 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (64.65%) - LeetSync

--- 404-sum-of-left-leaves/sum-of-left-leaves.java
+++ 404-sum-of-left-leaves/sum-of-left-leaves.java
@@ -16,29 +16,19 @@
 class Solution {
     public int sumOfLeftLeaves(TreeNode root) {
         
-        if(root==null || (root.left==null && root.right==null))
-        {
-            return 0;
-        }
-        int sum=0;
-       return check(root);
-        
-    }
-    public int check(TreeNode root)
-    {
-       
         if(root==null)
         {
             return 0;
         }
-         int sum=0;
+        int sum=0;
         if(root.left!=null && root.left.left==null && root.left.right==null)
         {
             sum+=root.left.val;
         }
-        sum+=check(root.left);
-        sum+=check(root.right);
 
+        sum+=sumOfLeftLeaves(root.left);
+        sum+=sumOfLeftLeaves(root.right);    
+    
         return sum;
     }
     
