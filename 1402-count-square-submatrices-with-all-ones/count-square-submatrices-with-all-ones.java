75e14cbcbc760c2a5c1a6fc41c359e273396632e
Kesanakurthy Tejesh sai manikanta <148753229+Tejesh916k@users.noreply.github.com>
2025-08-20 18:58:20 +0530

    Time: 6 ms (75.00%) | Memory: 55 MB (72.85%) - LeetSync

--- 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.java
+++ 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.java
@@ -7,33 +7,28 @@ class Solution {
 
         for(int i=0;i<m;i++) {
             mat[0][i]=matrix[0][i];
-            if(i==0) continue;
             sum+=matrix[0][i];
             }
-            System.out.println(sum);
-        for(int i=0;i<n;i++) {
+           
+        for(int i=1;i<n;i++) {
             mat[i][0]=matrix[i][0];
              sum+=matrix[i][0];
            
             }
-            System.out.println(sum);
+            
 
         for(int i=1;i<n;i++)
         {
             for(int j=1;j<m;j++)
             {
-                if(matrix[i][j]==0) continue;
-                mat[i][j]=matrix[i][j]+Math.min(mat[i-1][j-1],Math.min(mat[i][j-1],mat[i-1][j]));
+                if(matrix[i][j]==1){
+
+                mat[i][j]=1+Math.min(mat[i-1][j-1],Math.min(mat[i][j-1],mat[i-1][j]));
                 sum+=mat[i][j];
+                }
             }
         }
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<m;j++)
-            {
-                System.out.print(mat[i][j]);
-            }
-        }
+        
         
        
       return sum;  
