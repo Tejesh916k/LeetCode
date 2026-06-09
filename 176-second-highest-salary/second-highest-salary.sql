4ce8b8e7df622a013c3a01338cd5f2764bbf8171
Kesanakurthy Tejesh sai manikanta <148753229+Tejesh916k@users.noreply.github.com>
2026-06-09 12:49:52 +0530

    Time: 263 ms (91.88%) | Memory: 0B (100.00%) - LeetSync

--- 176-second-highest-salary/second-highest-salary.sql
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -1,6 +1,13 @@
 
-select max(salary) as SecondHighestSalary from
-(select salary from Employee
-group by salary
-order by salary desc
-limit 1 offset 1)as high
+
+select max(salary) as SecondHighestSalary from (
+    select salary from Employee group by salary 
+    order by salary desc limit 1 offset 1
+) as h;
+
+
+
+
+
+
+
